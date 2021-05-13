9fd6242fa17bc187cce212b2ed570d229dafaf3f
LAPTOP-R9A30I29\sergi <sbosche@esliceu.net>
2021-05-13 09:33:08 +0200

    no message

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 
+<body style="background-color:Cornsilk;">
     <div style="width: 100%; text-align: center">
         <h1>
             <i>
