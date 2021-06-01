67ddcb15de0254a9fcb38dbe69358ac4da9ac0c5
LAPTOP-R9A30I29\sergi <sbosche@esliceu.net>
2021-06-01 11:15:47 +0200

    4.0.1

--- insert_alumne.php
+++ insert_alumne.php
@@ -10,17 +10,28 @@
 
 
     <?php
-    $nom = '';
+    $DNI = '';
+    $nombre = '';
+    $cognom = '';
     $adreca = '';
-
+    $codi_postal = '';
+    $naixament = '';
+    $telefon = '';
+    
 
     if(isset($_GET['DNI'])){
-      $query = "select";
+      $query = "SELECT * from alumne WHERE DNI = $_GET[DNI]";
       $result = mysqli_query($bbdd, $query) or die (mysqli_error($bbdd));
       $alumne = mysqli_fetch_assoc($result);
-      $nom = $alumne['nom'];
+      $DNI = $alumne['DNI'];
+      $nombre = $alumne['nombre'];
+      $cognom = $alumne['cognom'];
       $adreca = $alumne['adreca'];
- 
+      $codi_postal = $alumne['codi_postal'];
+      $naixament = $alumne['naixament'];
+      $telefon = $alumne['telefon'];
+
+      
     }
     ?>
 
@@ -30,28 +41,28 @@
     <p>Aqui abaix podeu posar les vostres dades per poder inscriurer-vos</p>
   
     <label><b> Nom </b></label>
-    <input type="text" maxlength="120" value="<?=$nom?>" required name="nombre_alumne" placeholder="Escriu aqui el teu nom" />
+    <input type="text" maxlength="120" value="<?=$nombre?>" required name="nombre_alumne" placeholder="Escriu aqui el teu nom" />
 
     <label><b>Cognom</b></label>
-    <input type="text" maxlength="100" required name="cognom_alumne" placeholder="Escriu aqui el teu cognom" />
+    <input type="text" maxlength="100" value="<?=$cognom?>" required name="cognom_alumne" placeholder="Escriu aqui el teu cognom" />
     <div>
       <label><b>Adreça</b></label>
-      <input type="text" maxlength="100" required name="adreca" placeholder="Escriu aqui la teva adreça" />
+      <input type="text" maxlength="100" value="<?=$adreca?>" required name="adreca" placeholder="Escriu aqui la teva adreça" />
       <label><b>Codi postal</b></label>
-      <input type="text" maxlength="100" required name="codi_postal" placeholder="Escriu aqui el teu codi postal" />
+      <input type="text" maxlength="100" value="<?=$codi_postal?>" required name="codi_postal" placeholder="Escriu aqui el teu codi postal" />
     </div>
     <div>
       <label><b>telefon</b></label>
-      <input type="text" maxlength="100" required name="telefon" placeholder="Escriu aqui el teu telefon" />
+      <input type="text" maxlength="100" value="<?=$telefon?>" required name="telefon" placeholder="Escriu aqui el teu telefon" />
 
-      <label><b>naixament</b></label>
-      <input type="text" maxlength="100" required name="naixament" placeholder="Escriu aqui la teva data de naixament" />
+      
+      <input type="text" maxlength="100" value="<?=$naixament?>" required name="naixament" placeholder="Escriu aqui la teva data de naixament" />
     </div>
     <div>
       <label><b>DNI</b></label>
-      <input type="text" maxlength="150" required name="DNI" placeholder="Escriu aqui el teu DNI" />
+      <input type="text" maxlength="150" value="<?=$DNI?>"required name="DNI" placeholder="Escriu aqui el teu DNI" />
       <label><b>Curs</b></label>
-      <select name="Curs">
+      <select name="Curs">      
         <option value="1">1r ESO</option>
         <option value="2">2n ESO</option>
         <option value="3">3r ESO</option>
