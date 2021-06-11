22689d08954cb4ed1db682f21d594aeeb189c2b9
LAPTOP-R9A30I29\sergi <sbosche@esliceu.net>
2021-06-11 13:47:37 +0200

    4.2.3

--- index.php
+++ index.php
@@ -51,16 +51,29 @@
       </ul>
   
       </div>
-        </div>
-        <img src="imagenes/claase.jpg" wdith="300ph" >
-        </img>
+        
         <div class="background2">
-        <h1>
-          Més Informació
-        </h1>
+          <h1>
+            Més Informació
+         </h1>
 
-        <p>Porgramadors:Sergi</p><a href="https://www.instagram.com/__sergibosch__/"> @__sergibosch__
-          </a> 
+            <p>Porgramadors:Sergi Bosch i Jordi Mayrata</p>
+            <div>
+              <a href="https://www.instagram.com/__sergibosch__/"> @__sergibosch__
+              </a> 
+              <a href="https://www.instagram.com/mayrata2003/">@mayrata2003
+              </a>
+            </div>
+            <div  class="box">
+              <img src="imagenes/esliceu.gif" width="300ph" >
+            </img>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3074.158671283472!2d2.6871463152315154!3d39.6011079794682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1297935d441c9571%3A0xa01dc49e4565742f!2sEscola%20Es%20Liceu!5e0!3m2!1ses!2ses!4v1623409603580!5m2!1ses!2ses" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+             
+              <img src="imagenes/797afe1351677d0ad8787224e6a5be2a.png" width="285px" >
+              <img src="imagenes/cec8a6239de29acbfcfee0ccec995b9f.png" width="285px" >
+
+
+
         </div>
     
     </body>
